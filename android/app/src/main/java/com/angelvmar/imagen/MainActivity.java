3ebe0606a97179c0b431b7f02b5a344cbd08687e
angelvmar <angelvmar@gmail.com>
2026-09-09 03:29:18 -0500

    Ignorar carpeta android y generar entorno limpio en Actions

--- android/app/src/main/java/com/angelvmar/imagen/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.angelvmar.imagen;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- android/app/src/main/java/com/tumarcaribe/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.tumarcaribe.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
